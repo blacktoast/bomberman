4d649e074d61b140358befa709ad2c14ef6d0a77
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-08-27 16:38:12 -0700

    Added wall sprites to game

--- js/app.js
+++ js/app.js
@@ -1,15 +1,43 @@
 var mainState = {
     preload: function(){
-
+        game.load.image('wall', 'assets/wall.png');
     },
 
     create: function(){
+        this.PIXEL_SIZE = 40;
+        this.BLOCK_COUNT = 15;
 
+        game.stage.backgroundColor = "#287800";
+
+        this.wallList = game.add.group();
+
+        this.createMap();
     },
 
     update: function(){
 
-    }
+    },
+
+    createMap: function(){
+        for (var x = 0; x < 15; x++) {
+            for (var y = 0; y < 15; y++) {
+                if(x % 2 == 1 && y % 2 == 1){
+                    this.addWall(x, y);
+                } else if(x < 2 && y < 2){
+                    // add nothing, space for grass
+                } else {
+                    if(Math.floor(Math.random() * 3)){
+                        // add brick
+                    }
+                }
+            }
+        }
+    },
+
+    addWall: function(x, y){
+        var wall = game.add.sprite(x * this.PIXEL_SIZE, y * this.PIXEL_SIZE, 'wall');
+        this.wallList.add(wall);
+    },
 };
 
 var game = new Phaser.Game(600, 600);
