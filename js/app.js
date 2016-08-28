0065001cc966f30752929c632cfbabb9d33b0cbb
kevinpagtakhan <kevpagtakhan@gmail.com>
2016-08-27 18:22:04 -0700

    Collition detection

--- js/app.js
+++ js/app.js
@@ -10,8 +10,8 @@ var mainState = {
         this.BLOCK_COUNT = 15;
 
         game.stage.backgroundColor = "#287800";
-
         game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.world.enableBody = true;
 
         this.wallList = game.add.group();
         this.brickList = game.add.group();
@@ -19,30 +19,42 @@ var mainState = {
         this.createMap();
         this.addPlayer();
 
-        var upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        var downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
-        var rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        var leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+        this.cursor = game.input.keyboard.createCursorKeys();
 
-        upKey.onDown.add(this.moveUp, this);
-        downKey.onDown.add(this.moveDown, this);
-        rightKey.onDown.add(this.moveRight, this);
-        leftKey.onDown.add(this.moveLeft, this);
     },
 
     update: function(){
 
+        if (this.cursor.down.isDown || this.cursor.up.isDown || this.cursor.right.isDown || this.cursor.left.isDown){
+            if (this.cursor.left.isDown)
+                this.player.body.velocity.x = -150;
+            if (this.cursor.right.isDown)
+                this.player.body.velocity.x = +150;
+            if (this.cursor.up.isDown)
+                this.player.body.velocity.y = -150;
+            if (this.cursor.down.isDown)
+                this.player.body.velocity.y = 150;
+        } else{
+            this.player.body.velocity.x = 0;
+            this.player.body.velocity.y = 0;
+        }
+
+        game.physics.arcade.collide(this.player, this.wallList);
+        game.physics.arcade.collide(this.player, this.brickList);
     },
 
     createMap: function(){
         for (var x = 0; x < 15; x++) {
             for (var y = 0; y < 15; y++) {
-                if(x % 2 == 1 && y % 2 == 1){
+                if (x === 0 || y === 0 || x == 14 || y == 14){
                     this.addWall(x, y);
-                } else if(x < 2 && y < 2){
+                }
+                else if(x % 2 === 0 && y % 2 === 0){
+                    this.addWall(x, y);
+                } else if(x < 3 && y < 3){
                     // add nothing, space for grass
                 } else {
-                    if(Math.floor(Math.random() * 3)){
+                    if(Math.floor(Math.random() * 2)){
                         this.addBrick(x, y);
                     }
                 }
@@ -51,7 +63,7 @@ var mainState = {
     },
 
     addPlayer: function(){
-        this.player = game.add.sprite(0, 0, 'bomber');
+        this.player = game.add.sprite(this.PIXEL_SIZE, this.PIXEL_SIZE, 'bomber');
         game.physics.arcade.enable(this.player);
 
     },
@@ -59,6 +71,7 @@ var mainState = {
     addWall: function(x, y){
         var wall = game.add.sprite(x * this.PIXEL_SIZE, y * this.PIXEL_SIZE, 'wall');
         game.physics.arcade.enable(wall);
+        wall.body.immovable = true;
         this.wallList.add(wall);
 
     },
@@ -66,32 +79,9 @@ var mainState = {
     addBrick: function(x, y){
         var brick = game.add.sprite(x * this.PIXEL_SIZE, y * this.PIXEL_SIZE, 'brick');
         game.physics.arcade.enable(brick);
+        brick.body.immovable = true;
         this.brickList.add(brick);
 
-    },
-
-    moveUp: function(){
-        if(this.player.y > 0){
-            this.player.y -= this.PIXEL_SIZE;
-        }
-    },
-
-    moveDown: function(){
-        if(this.player.y < (600 - this.PIXEL_SIZE)){
-            this.player.y += this.PIXEL_SIZE;
-        }
-    },
-
-    moveRight: function(){
-        if(this.player.x < (600 - this.PIXEL_SIZE)){
-            this.player.x += this.PIXEL_SIZE;
-        }
-    },
-
-    moveLeft: function(){
-        if(this.player.x > 0){
-            this.player.x -= this.PIXEL_SIZE;
-        }
     },
 };
 
